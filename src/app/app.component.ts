673e18e9e741a8bd97c15e26e28d7e3ff3f56ef4
michele malagnini <mmalagnini@pmeasuring.com>
2025-04-22 20:27:32 +0200

    feat: implement scroll-to-top functionality on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,9 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet ],
 })
 export class AppComponent {
- 
+  constructor(router: Router) {
+    router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => window.scrollTo(0, 0));
+  }
 }
